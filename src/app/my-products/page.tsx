175d92010fd410ea30acb383e8811361651e7fe8
Ganesh Bommidi <bommidiganesh27@gmail.com>
2025-11-06 16:43:21 +0000

    in manifactor portel only do this when add new product when submit that

--- src/app/my-products/page.tsx
+++ src/app/my-products/page.tsx
@@ -129,7 +129,7 @@ export default function MyProductsPage() {
 
   return (
     <>
-      <main className="flex flex-1 flex-col gap-4">
+      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between">
             <div>
@@ -355,4 +355,3 @@ export default function MyProductsPage() {
   );
 }
 
-    
